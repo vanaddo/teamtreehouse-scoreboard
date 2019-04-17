ec5e0e38fbadf6dca3c7128dfae986a346b06e16
vanaddo <samea@Samanthas-MacBook-Pro.local>
2019-04-17 13:36:52 -0400

    Refactor

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 function Header() {
-    return (
+    return(
         <header>
             <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
+            <span classNmae="status">Players: 1</span>
         </header>
     );
     
 }
+
 ReactDOM.render(
     <Header />,
     document.getElementById('root')
