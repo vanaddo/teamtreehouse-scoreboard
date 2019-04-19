71b2c3ac6121c681defbf72d2866125a71557672
vanaddo <samea@Samanthas-MacBook-Pro.local>
2019-04-19 01:12:22 -0400

    Finally got the components to render in the browser

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 function Header() {
-    return(
-        <header>
-            <h1>Scoreboard</h1>
-            <span classNmae="status">Players: 1</span>
-        </header>
-    );
-    
+  return(
+    <header>
+      <h1>Scoreboard</h1>
+      <span className= "status">Players: 1</span>
+    </header>
+  );
 }
 
 ReactDOM.render(
-    <Header />,
-    document.getElementById('root')
+  <Header />,
+  document.getElementById("root")
 );
